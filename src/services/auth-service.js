ec0edd71c0a396901073619190f2d0d242600f57
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-05 22:03:13 -0400

    switch over to local development

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,25 +1,43 @@
 import axios from "axios";
 // const BASE_URL = process.env.REACT_APP_BASE_URL
-// const BASE_URL = "http://localhost:4000";
-const BASE_URL = "https://fse-sp22-juanong-a4.herokuapp.com"
-const AUTH_API = `${BASE_URL}/api/auth`
+const BASE_URL = "http://localhost:4000";
+// const BASE_URL = "https://fse-sp22-juanong-a4.herokuapp.com"
+// const AUTH_API = `${BASE_URL}/api/auth`
 
 const api = axios.create({
     withCredentials: true
 });
 
 export const signup = (user) =>
-    api.post(`${AUTH_API}/signup`, user)
+    api.post(`${BASE_URL}/signup`, user)
         .then(response => response.data);
 
 export const profile = () =>
-    api.post(`${AUTH_API}/profile`)
+    api.post(`${BASE_URL}/profile`)
         .then(response => response.data);
 
 export const logout = (user) =>
-    api.post(`${AUTH_API}/logout`, user)
+    api.post(`${BASE_URL}/logout`, user)
         .then(response => response.data);
 
 export const login = (credentials) =>
-    api.post(`${AUTH_API}/login`, credentials)
+    api.post(`${BASE_URL}/login`, credentials)
         .then(response => response.data);
+
+
+
+// export const signup = (user) =>
+//     api.post(`${AUTH_API}/signup`, user)
+//         .then(response => response.data);
+
+// export const profile = () =>
+//     api.post(`${AUTH_API}/profile`)
+//         .then(response => response.data);
+
+// export const logout = (user) =>
+//     api.post(`${AUTH_API}/logout`, user)
+//         .then(response => response.data);
+
+// export const login = (credentials) =>
+//     api.post(`${AUTH_API}/login`, credentials)
+//         .then(response => response.data);
