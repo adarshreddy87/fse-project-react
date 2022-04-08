8276e6ba803edd4c2a275c5ecf788ab603de91f5
Juan Ong <juanong26@gmail.com>
2022-04-08 13:22:40 -0400

    get logged in user details in home screen

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Tuits from "../tuits";
 import * as service from "../../services/tuits-service";
+import * as authService from '../../services/auth-service';
 import {useEffect, useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
+import {useLocation, useParams, useNavigate} from "react-router-dom";
+import './home.css'
 
 const Home = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const {uid} = useParams();
   const [tuits, setTuits] = useState([]);
   const [tuit, setTuit] = useState('');
-  const userId = uid;
+  const [profile, setProfile] = useState({});
   const findTuits = () => {
     if(uid) {
       return service.findTuitsByUser(uid)
@@ -19,55 +22,37 @@ const Home = () => {
         .then(tuits => setTuits(tuits))
     }
   }
+
+  // A user must be logged in to see the home screen
+  useEffect(async () => {
+    try {
+      const loggedInUser = await authService.profile();
+      setProfile(loggedInUser);
+    } catch (e) {
+      navigate('/login');
+    }
+  }, [])
+
   useEffect(() => {
     let isMounted = true;
     findTuits()
     return () => {isMounted = false;}
   }, []);
-  const createTuit = () =>
-      service.createTuit(userId, {tuit})
-          .then(findTuits)
+  // const createTuit = () =>
+  //     service.createTuit(userId, {tuit})
+  //         .then(findTuits)
   const deleteTuit = (tid) =>
       service.deleteTuit(tid)
           .then(findTuits)
   // refresh tuits needs to be tweaked
+  console.log(profile);
   return(
     <div className="ttr-home">
       <div className="border border-bottom-0">
         <h4 className="fw-bold p-2">Home Screen</h4>
-        {
-          uid &&
-          <div className="d-flex">
-            <div className="p-2">
-              <img className="ttr-width-50px rounded-circle"
-                   src="../images/nasa-logo.jpg"/>
-            </div>
-            <div className="p-2 w-100">
-              <textarea
-                  onChange={(e) =>
-                      setTuit(e.target.value)}
-                placeholder="What's happening?"
-                className="w-100 border-0"></textarea>
-              <div className="row">
-                <div className="col-10 ttr-font-size-150pc text-primary">
-                  <i className="fas fa-portrait me-3"></i>
-                  <i className="far fa-gif me-3"></i>
-                  <i className="far fa-bar-chart me-3"></i>
-                  <i className="far fa-face-smile me-3"></i>
-                  <i className="far fa-calendar me-3"></i>
-                  <i className="far fa-map-location me-3"></i>
-                </div>
-                <div className="col-2">
-                  <a onClick={createTuit}
-                     className={`btn btn-primary rounded-pill fa-pull-right
-                                  fw-bold ps-4 pe-4`}>
-                    Tuit
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        }
+      </div>
+      <div className="border negative-top-margin row-container">
+        <h4 className="fw-bold p-2">Home Screen</h4>
       </div>
       <Tuits tuits={tuits} deleteTuit={deleteTuit} refreshTuits={findTuits}/>
     </div>
