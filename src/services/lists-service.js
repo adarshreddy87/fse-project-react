8dc72f9464065896fe658b63f38b3e5d52d09a16
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-04-23 19:21:09 -0400

    Implemented service and front end for lists - includes implementation of my users list, all users list and my users tuits

---
+++ src/services/lists-service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000";
+const USERS_API = `${BASE_URL}/api/users`;
+
+const api = axios.create({
+                             withCredentials: true
+                         });
+
+export const getAllUsersInList = (uid) =>
+    api.get(`${USERS_API}/${uid}/lists`)
+        .then(response => response.data);
+
+export const removeUserFromList = (uid, userId) =>
+    api.delete(`${USERS_API}/${uid}/lists/${userId}`)
+        .then(response => response.data);
+
+export const addUserToList = (uid, userId) =>
+    api.post(`${USERS_API}/${uid}/lists/${userId}`)
+        .then(response => response.data);
+
+export const getAllUsersNotInList = (uid) =>
+    api.get(`${USERS_API}/${uid}/not/lists`)
+        .then(response => response.data);
+
+export const getAllUsersTuitsInList = (uid) =>
+    api.get(`${USERS_API}/${uid}/lists/tuits`)
+        .then(response => response.data);
+
