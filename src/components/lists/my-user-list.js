337186afc4d69fed53a9c2455d06ea18e65dc889
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-04-24 00:12:21 -0400

    Implemented add user and remove user functionalities.

--- src/components/lists/my-user-list.js
+++ src/components/lists/my-user-list.js
@@ -2,14 +2,15 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import * as listService from "../../services/lists-service";
 
-const MyUserList = () => {
+const MyUserList = ({deleteUser}) => {
     const [users, setUsers] = useState([]);
     const allUsersInList = () =>
         listService.getAllUsersInList("me")
             .then((users) => setUsers(users));
-    const removeUser = (userId) =>
-        listService.removeUserFromList("me", userId)
     useEffect(allUsersInList,[]);
+    const removeUser = (userId) =>
+        listService.removeUserFromList("me", userId).then(allUsersInList)
+
     return (
         <div>
             <Link to="/lists" classname='nav-link active'>
@@ -23,16 +24,15 @@ const MyUserList = () => {
                             <Link className="list-group-item"
                                   key={user.addedUser._id}
                                   to={`/home/${user.addedUser._id}`}>
-              <span className="fs-3">
+              <span className="fs-5">
                 {user.addedUser.username}
               </span>
-                                <button onClick={(e) => {
+                                <i onClick={(e) => {
                                     e.stopPropagation()
                                     e.preventDefault()
                                     removeUser(user.addedUser._id)
-                                }} className="btn btn-danger fa-pull-right">
-                                    <i className="fas fa-remove"></i>
-                                </button>
+                                }} className="btn btn-outline-danger fas fa-remove fa-pull-right">
+                                </i>
                             </Link>
                         )
                     })
